82d54e4de04feb59132c547f7157afb7e550baa1
NotACat1 <notacat.mail@gmail.com>
2023-10-03 16:59:32 +0400

    feat: добавление констант для JS файлов

---
+++ src/utills/constants.js
@@ -0,0 +1,7 @@
+export const configForm = {
+  inputSel: '.form__input',
+  subBtnSel: '.form__button',
+  textBtnSel: '.form__button-text',
+  iconBtnSel: '.form__button-icon',
+  dataTextSuc: 'successText',
+};
